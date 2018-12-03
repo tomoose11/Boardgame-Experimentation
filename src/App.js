9d4f5cc66f3efa45831fbe51f532e0c5f3d1e221
tomoose11 <tomwall_84@hotmail.co.uk>
2018-12-03 22:43:02 +0000

    player turns

--- src/App.js
+++ src/App.js
@@ -8,7 +8,9 @@ import Roll from './components/Roll';
 class App extends Component {
   state = {
     player1pos: 63,
-    roll: 0
+    player2pos: 63,
+    roll: 0,
+    p1turn: true
   };
   componentDidMount = () => {
     let index = 64;
@@ -29,18 +31,16 @@ class App extends Component {
     ].getBoundingClientRect().left - 753}px,${tiles[
       this.state.player1pos
     ].getBoundingClientRect().top - 363}px)`;
-  };
 
-  componentDidUpdate = () => {
-    // const tiles = document.getElementsByClassName('tile');
-    // document.getElementsByClassName(
-    //   'player'
-    // )[0].style.transform = `translate(${tiles[
-    //   this.state.player1pos
-    // ].getBoundingClientRect().left - 753}px,${tiles[
-    //   this.state.player1pos
-    // ].getBoundingClientRect().top - 363}px)`;
-    this.handleMove(this.state.roll, this.state.player1pos + this.state.roll);
+    document.getElementsByClassName(
+      'player'
+    )[1].style.transform = `translate(${tiles[
+      this.state.player2pos
+    ].getBoundingClientRect().left - 783}px,${tiles[
+      this.state.player2pos
+    ].getBoundingClientRect().top - 363}px)`;
+    document.getElementsByClassName('player')[1].style.backgroundColor =
+      'white';
   };
 
   render() {
@@ -48,33 +48,51 @@ class App extends Component {
       <div className="App">
         <Board />
         <Player />
+        <Player />
         <Roll handleRoll={this.handleRoll} />
       </div>
     );
   }
 
   handleRoll = () => {
-    console.log('rolled!');
-    const roll = Math.floor(Math.random() * 6);
-    this.setState(prevState => ({
-      player1pos: prevState.player1pos - roll,
-      roll: roll
-    }));
+    if (this.state.p1turn) {
+      this.handleWhichPlayer('player1pos', 0);
+    } else {
+      this.handleWhichPlayer('player2pos', 1);
+    }
   };
 
-  handleMove = (roll, pos) => {
+  handleWhichPlayer = (player, pindex) => {
+    const roll = Math.floor(Math.random() * 6) + 1;
+    this.setState(
+      prevState => ({
+        [player]: prevState[player] - roll,
+        roll: roll + 1
+      }),
+      () => {
+        this.handleMove(
+          this.state.roll,
+          this.state[player] + this.state.roll,
+          pindex
+        );
+        this.setState({
+          p1turn: !this.state.p1turn
+        });
+      }
+    );
+  };
+
+  handleMove = (roll, pos, player) => {
     setTimeout(() => {
       if (roll > 0) {
         const tiles = document.getElementsByClassName('tile');
-        document.getElementsByClassName(
-          'player'
-        )[0].style.transform = `translate(${tiles[
-          pos - 1
-        ].getBoundingClientRect().left - 753}px,${tiles[
-          pos - 1
-        ].getBoundingClientRect().top - 363}px)`;
+        document.getElementsByClassName('player')[
+          player
+        ].style.transform = `translate(${tiles[pos - 1].getBoundingClientRect()
+          .left - 753}px,${tiles[pos - 1].getBoundingClientRect().top -
+          363}px)`;
         roll--;
-        this.handleMove(roll, pos - 1);
+        this.handleMove(roll, pos - 1, player);
       }
     }, 500);
   };
